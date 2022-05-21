13c932fff3826e4e0eff9a8dcc2b00693f695a54
tridence <brianwanjalabarasa@gmail.com>
2022-05-21 10:24:38 +0300

    Increment

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
-// document.getElementById ("count-el").innerText = 5
-
+// intialize the count as 0
 let count = 0
+let countEl = document.getElementById("count-el")
 
-console.log(count)
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+function increment() {
+    count = count + 1
+    document.getElementById ("count").innerText = 100
+    console.log(count)
+}
 
-// // 1. Create a variable, myAge and set its value to your ge
-// let myAge = 22
-
-// // 2. Log the myAge variable to the console
-// console.log(myAge)
-
-// 1. Create two variables, myAge and humanDogRatio
-let myAge = 25
-let humanDogRatio = 7
-
-
-// 2. Multiply the two together and store the result in myDogAge 
-let myDogAge = myAge * humanDogRatio
-
-// 3. Log myDogAge to the console
-console.log(myDogAge)
-
-// Reassigning and incrementing
-let boy = 6
-boy = 7
-boy = 23
-
-// Incrementing
- boy =boy + 4
- 
-console.log(boy)
-
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-// Decrease it down to 25, and then finally increase it to 70
- bonusPoints = bonusPoints - 75
- console.log(bonusPoints)
-
- bonusPoints = bonusPoints + 45
- console.log(bonusPoints)
+// change the count-el in the HTML to reflect the new count
