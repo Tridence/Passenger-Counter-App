f756a6f5b3060754cbe92d7205d186830357abe6
tridence <brianwanjalabarasa@gmail.com>
2022-05-21 09:12:19 +0300

    dogAge Conversion

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ let count = 0
 
 console.log(count)
 
-// 1. Create a variable, myAge and set its value to your ge
-let myAge = 22
+// // 1. Create a variable, myAge and set its value to your ge
+// let myAge = 22
 
-// 2. Log the myAge variable to the console
-console.log(myAge)
+// // 2. Log the myAge variable to the console
+// console.log(myAge)
+
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 25
+let humanDogRatio = 1/7
+
+
+// 2. Multiply the two together and store the result in myDogAge 
+let myDogAge = myAge * humanDogRatio
+
+// 3. Log myDogAge to the console
+console.log(myDogAge)
